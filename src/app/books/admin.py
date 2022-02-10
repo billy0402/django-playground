2ae3ace729d1ff66fd8ce1eb7c0cbfa14bea95bb
Billy Huang <10646003@ntub.edu.tw>
2022-02-11 02:02:59 +0800

    feat(books): create models

--- src/app/books/admin.py
+++ src/app/books/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+
+from .models import Author, Publisher, Classification, Tag, Book
+
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Classification)
+admin.site.register(Tag)
+admin.site.register(Book)
