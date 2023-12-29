1328c9f27ff8cedf4e596644e2a74bc4e8e18f68
Billy Huang <10646003@ntub.edu.tw>
2023-12-29 23:53:37 +0800

    feat(settings): setup cores allowed origins

--- src/core/settings.py
+++ src/core/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS: list[str] = []
 
+CORS_ALLOWED_ORIGINS: list[str] = []
+
 
 # Application definition
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'django_bootstrap5',
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
